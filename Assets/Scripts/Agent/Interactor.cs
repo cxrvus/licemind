6a030d81a03d1d7d197a9821a7a2951c52d1b499
cxrvus <benito.m.rabe@gmail.com>
2024-10-15 01:20:04 +0200

    fixed interaction hover, only targeting the closest one

--- Assets/Scripts/Agent/Interactor.cs
+++ Assets/Scripts/Agent/Interactor.cs
@@ -1,19 +1,48 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
-	public void OnTriggerEnter2D(Collider2D collision) {
+
+	Interactive closestInteractive;
+	readonly List<Interactive> interactives = new ();
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
 		if(collision.TryGetComponent<Interactive>(out var interactive))
 		{
-			interactive.InteractionHover(true);
+			HandleTrigger(true, interactive);
 		}
 	}
 
-	public void OnTriggerExit2D(Collider2D collision) {
+	public void OnTriggerExit2D(Collider2D collision)
+	{
 		if(collision.TryGetComponent<Interactive>(out var interactive))
 		{
-			interactive.InteractionHover(false);
+			HandleTrigger(false, interactive);
 		}
+	}
+
+	void HandleTrigger(bool entering, Interactive interactive)
+	{
+		if (entering)
+		{
+			interactives.Add(interactive);
+		}
+		else
+		{
+			interactive.InteractionHover(false);
+			interactives.Remove(interactive);
+		}
+
+		if (interactives.Count == 0) return;
+
+		closestInteractive = interactives.OrderBy(x => (transform.parent.position - x.transform.position).sqrMagnitude).First();
+		var otherInteractives = interactives.Where(x => x != closestInteractive).ToList();
+
+		if (closestInteractive) closestInteractive.InteractionHover(true);
+		otherInteractives.ForEach(x => x.InteractionHover(false));
 	}
 }
