26222969c06f9e06d00b29e8759f5868ca662c4d
cxrvus <benito.m.rabe@gmail.com>
2024-10-14 20:06:59 +0200

    implemented louse animation

--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -2,27 +2,35 @@ using UnityEngine;
 
 public class AgentMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Vector3 Direction { get; private set; }
+	public bool IsMoving { get { return Direction.magnitude > 0; } }
 
 	public bool isPlayer;
 	public float speed;
 	const float SPEED_FACTOR = 100;
 
-    // Update is called once per frame
+	Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
 	void FixedUpdate()
 	{
-		Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized;
+		Direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized;
 		Rigidbody2D rb = GetComponent<Rigidbody2D>();
-		rb.velocity = speed * SPEED_FACTOR * Time.deltaTime * direction;
+		rb.velocity = speed * SPEED_FACTOR * Time.deltaTime * Direction;
 
-		if (direction.magnitude > 0)
+		if (IsMoving)
 		{
-			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+			float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg - 90;
 			transform.rotation = Quaternion.Euler(0, 0, angle);
+			animator.Play("Walk");
+		}
+		else
+		{
+			animator.Play("Idle");
 		}
 	}
 }
