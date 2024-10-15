bfe71dec6df3125f31c9853004c6c6ff8850a2ec
cxrvus <benito.m.rabe@gmail.com>
2024-10-15 15:06:45 +0200

    implemented hair destruction

--- Assets/Scripts/Interactive/Hair.cs
+++ Assets/Scripts/Interactive/Hair.cs
@@ -2,8 +2,33 @@ using UnityEngine;
 
 public class Hair : Interactive
 {
+	public int maxDurability = 1;
+	int durability;
+	SpriteRenderer sprite;
+
+	public void Awake()
+	{
+		durability = maxDurability;
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
 	protected override void OnInteract(Agent agent)
 	{
-		Debug.Log("bitin da fuck outa that hair rn");
+		// todo: agent strength factor
+		durability--;
+		SetTransparency();
+		if (durability <= 0) Destroy(gameObject);
+	}
+
+	void SetTransparency()
+	{
+		const float BASE_TRANSP = 0.8f;
+		const float TRANSP_FACTOR = 1f - BASE_TRANSP;
+
+		var ratio = (float)durability / maxDurability * TRANSP_FACTOR + BASE_TRANSP;
+
+		var color = sprite.color;
+		color.a = ratio;
+		sprite.color = color;
 	}
 }
