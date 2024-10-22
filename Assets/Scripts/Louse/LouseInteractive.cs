b9fade43f45bad64988e913bab12a2f9a7c9868b
cxrvus <benito.m.rabe@gmail.com>
2024-10-22 13:18:46 -0400

    misc fix

--- Assets/Scripts/Louse/LouseInteractive.cs
+++ Assets/Scripts/Louse/LouseInteractive.cs
@@ -3,6 +3,6 @@ public class LouseInteractive : Interactive
 	public override void Interact(LouseStats other)
 	{
 		other.BecomeNpc();
-		GetComponent<LouseStats>().BecomeNpc();
+		GetComponent<LouseStats>().BecomePlayer();
 	}
 }
