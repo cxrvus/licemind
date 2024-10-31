cf0aaf15a176c3824a68b6cc96a7e12f76be6bf1
cxrvus <benito.m.rabe@gmail.com>
2024-10-31 19:07:23 +0100

    fixed back-and-forth player switching

--- Assets/_Project/Louse/Scripts/LouseInteractive.cs
+++ Assets/_Project/Louse/Scripts/LouseInteractive.cs
@@ -1,5 +1,14 @@
+using System.Collections;
+using UnityEngine;
+
 public class LouseInteractive : Interactive
 {
 	public override bool CanInteract(Louse other) => other.IsPlayer;
-	public override void Interact(Louse _) => GetComponent<Louse>().IsPlayer = true;
+	public override void Interact(Louse _) => StartCoroutine(SwitchPlayer());
+	
+	IEnumerator SwitchPlayer()
+	{
+		yield return new WaitForSeconds(1);
+		GetComponent<Louse>().IsPlayer = true;
+	}
 }
